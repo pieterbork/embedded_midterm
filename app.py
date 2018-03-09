767483cd6f75d36165a9595c85b90dc34e16d7d2
Ryan Bohannon <ryan.m.bohannon@gmail.com>
2018-03-09 13:23:55 -0700

    remove extra pages

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/camp')
+@app.route('/blank')
 def camp():
-    return render_template('camp.html')
+    return render_template('blank.html')
 
 @app.route('/charts')
 def charts():
